332e30c8671c35d3e7e5f718da34911aede3f596
Ben Halverson <benhalverson33@gmail.com>
2019-10-07 23:29:43 -0700

    setup express server

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/', indexRouter);
+
+module.exports = app;
